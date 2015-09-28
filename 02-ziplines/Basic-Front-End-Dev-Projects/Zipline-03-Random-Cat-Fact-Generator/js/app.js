d40f8e21810e19370e3ce876203c7f65159880c1
Brigette Eckert <brigette.eckert@live.com>
2015-09-27 17:16:57 -0700

    got API working on at fact generator and click to show fact working, working on tweet button. Set URL encoded html for fact

---
+++ 02-ziplines/Basic-Front-End-Dev-Projects/Zipline-03-Random-Cat-Fact-Generator/js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+		var url = "http://crossorigin.me/http://catfacts-api.appspot.com/api/facts?number=1?callback=JSON_CALLBACK?"
+		var twitterHTML = "https://twitter.com/intent/tweet?text=";
+		var fact = "";
+		var factHTML = "";
+		var tweetURL = "";
+		//twitter button js
+		window.twttr = (function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0],
+						t = window.twttr || {};
+				if (d.getElementById(id)) return t;
+				js = d.createElement(s);
+				js.id = id;
+				js.src = "https://platform.twitter.com/widgets.js";
+				fjs.parentNode.insertBefore(js, fjs);
+
+				t._e = [];
+				t.ready = function(f) {
+						t._e.push(f);
+				};
+
+				return t;
+		}(document, "script", "twitter-wjs"));
+
+		//fact call back
+		$("#fact-btn").click(function() {
+				$.getJSON(url, function(data) {
+						fact = data.facts[0];
+						$("#fact-display").html(fact)
+						factHTML = encodeURI(fact)
+					tweetURL= twitterHTML + factHTML;
+					console.log(tweetURL);
+				});
+
+				$(".btn-o").click(function() {
+						console.log("clicked");
+				});
+		});
+
+})
