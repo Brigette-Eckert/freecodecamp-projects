b92b3c665b08700496cf903da3c13165da154e04
Brigette Eckert <brigette.eckert@live.com>
2015-10-09 18:19:09 -0700

    added some css and moved local weather app to intermediate projets because of fcc move

---
+++ 02-ziplines/Intermediate-Front-End-Dev-Projects/01-Weather-App/js/app.js
@@ -0,0 +1,9 @@
+// find weather api
+
+
+// change backgroubd based on weather 
+
+// hit enter to submit as well as pushing button
+// tab to get to next piece of form 
+
+// find intnial background
