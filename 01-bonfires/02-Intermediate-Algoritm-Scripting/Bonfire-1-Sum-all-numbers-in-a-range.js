70276648910a18f466e1b717df303d4d186593cd
Brigette Eckert <brigette.eckert@live.com>
2015-10-09 19:28:14 -0700

    completed intermediate bonfire 1

--- 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-1-Sum-all-numbers-in-a-range.js
+++ 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-1-Sum-all-numbers-in-a-range.js
@@ -2,7 +2,23 @@
 // The lowest number will not always come first.
 
 function sumAll(arr) {
-  return 1;
+	//find lower number
+	var rangeLow = Math.min.apply(Math, arr);
+	//find higher number
+	var rangeHigh = Math.max.apply(Math, arr);
+	//create range of numbers
+	var allNum = [];
+		for(i=rangeLow; i <= rangeHigh; i++) {
+			allNum.push(i);
+		};
+	console.log(allNum);
+	//sum of array of numbers in range
+
+	var sum = allNum.reduce(function(a,b) {
+		 	return a+b;
+	});
+	console.log(sum);
+	return sum;
 }
 
 sumAll([1, 4]);
