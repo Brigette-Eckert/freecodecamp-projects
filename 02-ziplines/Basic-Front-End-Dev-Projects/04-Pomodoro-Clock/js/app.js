ca2a7e964cb4eb97f1dd135da1c65f9c7198196d
Brigette Eckert <brigette.eckert@live.com>
2015-12-01 20:59:46 -0800

    added setinterval function and start/reset buttons to pomorodo clock, clock counting down when start clicked

--- 02-ziplines/Basic-Front-End-Dev-Projects/04-Pomodoro-Clock/js/app.js
+++ 02-ziplines/Basic-Front-End-Dev-Projects/04-Pomodoro-Clock/js/app.js
@@ -1,49 +1,88 @@
-//set initinal vars 
-var workTime = 25;
-var breakTime =  5;
+//set init var
+var time = {
+  work: 25,
+  break: 5
+};
+var count = time.work;
 //set inner html to respective varibles
-$("p.workInt").html(workTime);
-$("p.breakInt").html(breakTime);
+$("p.workInt").html(time.work);
+$("p.breakInt").html(time.break);
+$("p.counter").html(count);
+$("#start").html("Start");
 
-$('#addWork').click(function(){
+$('#addWork').click(function() {
   //on + button click add 1 to Work     interval
-   workTime +=1;
-  $("p.workInt").html(workTime);
-  console.log(workTime);
-}); 
+  time.work += 1;
+  count = time.work;
+  $("p.workInt").html(time.work);
+  //updating count var to change with work
+  $("p.counter").html(count);
+});
 
-$('#minusWork').click(function(){
+$('#minusWork').click(function() {
   //on - button click subtract 1 to Work interval
-    workTime -=1;
-  if(workTime >= 0) {
-  $("p.workInt").html(workTime);
-  console.log(workTime);
+  time.work -= 1;
+  count = time.work;
+  //avoiding non-existant and negative intervals
+  if (time.work >= 1) {
+    //updating count var to change with work
+    $("p.workInt").html(time.work);
+    $("p.counter").html(count);
+
   } else {
-    workTime = 0;
+    time.work = 1;
+    count = time.work;
   }
-}); 
+});
 
-$('#addBreak').click(function(){
-   //on + button click add 1 to Break     interval
-   breakTime +=1;
-  //dry out
-  $("p.breakInt").html(breakTime);
-  console.log(breakTime);
-}); 
+$('#addBreak').click(function() {
+  //on + button click add 1 to Break     interval
+  time.break += 1;
+  $("p.breakInt").html(time.break);
+  console.log(time.break);
+});
 
-
-$('#minusBreak').click(function(){
+$('#minusBreak').click(function() {
   //on - button click subtract one from break interval
-    breakTime -=1;
-  if(breakTime >=0){
-  $("p.breakInt").html(breakTime);
-  console.log(breakTime);
+  time.break -= 1;
+  //avoiding non-existant and negative intervals
+  if (time.break >= 1) {
+    $("p.breakInt").html(time.break);
+    console.log(time.break);
   } else {
-    breakTime = 0;
+    time.break = 1;
   }
-}); 
+});
 
-//click to start and stop clock
+//reset count to work time
+$("#reset").click(function() {
+  count = time.work;
+  console.log("timer reset")
+});
+
+$("#reset").click(function() {
+  count = time.work;
+  console.log("timer reset")
+});
+
+function countDown() {
+  console.log("countdown started");
+  count -= 1;
+  console.log(count);
+    if(count <= 0){
+    console.log("stop");
+    }
+};
+
+var timer = setInterval(countDown, 1000)
+
+$("#start").click(function() {
+  console.log("timer started");
+  timer();
+});
+
+//click to start and stop clock use inner html and active class to change word from start to stop
 //set time interval
-//rest button
-//reset clock when work timer changed 
+//add active class to determine when clock should show work or break time
+//add sound when swtiches from work to break and vice versa
+//reset clock when work timer changed
