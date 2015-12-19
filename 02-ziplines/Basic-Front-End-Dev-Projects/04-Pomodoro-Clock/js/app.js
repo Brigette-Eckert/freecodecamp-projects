d0c4bc32305eeb5bec010b132943ed544da90e89
Brigette Eckert <brigette.eckert@live.com>
2015-12-19 09:04:37 -0800

    pre push commit

--- 02-ziplines/Basic-Front-End-Dev-Projects/04-Pomodoro-Clock/js/app.js
+++ 02-ziplines/Basic-Front-End-Dev-Projects/04-Pomodoro-Clock/js/app.js
@@ -14,52 +14,55 @@ $("p.workInt").html(time.work);
 $("p.breakInt").html(time.break);
 $("p.type").html("Work");
 $("#start-stop").html("Start");
+$("p.counter").html(count);
 
-function timeDisplay() {
-  console.log("the count is" + " " + count);
+function timeDisplay(timeInput) {
+  console.log("the count is" + " " + timeInput);
   var min;
   var sec;
    //set dispaly to xx:xx vs one string  
-  if(count >= 60){
-     min = Math.floor(count/60);
-     sec = count%60;
+  if(timeInput >= 60){
+     min = Math.floor(timeInput/60);
+     sec = timeInput%60;
      //setting dispaly so 0s are in correct place when needed
       if(sec == 0) {
         count  = min.toPrecision(2) + ":" + sec + "0";
       } else if(min < 10) {
           count  = "0" + min + ":" + sec;
       } else if(min == 0) {
+        //fix seconds display with other else if &&
           count  = "00" + ":" + sec;
       } else {
         count  = min + ":" + sec;
       }
-     
   } else {
     min = 0;
-    sec = count;
+    sec = timeInput;
     count  = "0" + min + ":" + sec.toPrecision(2);
    }
-
    $("p.counter").html(count);
-
 };
 
 function addWork() {
-     time.work += 1;
+    time.work += 1;
     count = time.work;
-    $("p.workInt").html(time.work);
+    timeDisplay(count);
+    $("p.workInt").html(count);
     //updating count var to change with work
-    timeDisplay();
+    $("p.counter").html(count);
   };
 
 function minusWork() {
     //on - button click subtract 1 to Work interval
     time.work -= 1;
     count = time.work;
+    //not sure why this is coming up as undefine
+    timeDisplay(count);
+     $("p.workInt").html(count);
     //avoiding non-existant and negative intervals
     if(time.work >= 1) {
       //updating count var to change with work
-      $("p.workInt").html(time.work);
+      $("p.workInt").html(count);
       timeDisplay();
     } else {
       time.work = 1;
@@ -72,7 +75,6 @@ function addBreak() {
     //on + button click add 1 to Break     interval
     time.break += 1;
     $("p.breakInt").html(time.break);
-    timeDisplay();
   };
 
 function minusBreak() {
@@ -81,7 +83,6 @@ function minusBreak() {
   //avoiding non-existant and negative intervals
   if (time.break >= 1) {
     $("p.breakInt").html(time.break);
-    timeDisplay();
   } else {
     time.break = 1;
   }
@@ -92,7 +93,6 @@ function reset() {
     $("#clock").addClass("workTime");
     }
     count = time.work;
-    timeDisplay();
     console.log("timer reset")    
 
   };
@@ -114,6 +114,7 @@ function checkStatus() {
 function countDown(){
     if (count >= 1) {
       count -= 1;
+      console.log(count);
       $("p.counter").html(count);
     } else if(count == 0)  {
       console.log("switch")
@@ -126,7 +127,7 @@ function countDown(){
   };
   
 
-timeDisplay(); 
+timeDisplay(count); 
 //when calling timeDisplay function here functions break
 
 $('#addWork').click(function(){
@@ -153,7 +154,7 @@ $("#reset").click(function() {
 
 $("#start-stop").click(function() {
   $("#start-stop").toggleClass("running");
-    console.log("timer started");
+    console.log("timer toggled");
     //Toggling start and stop button
     if ($("#start-stop").hasClass("running") == true) {
      timer = setInterval(function(){ countDown()}, 1000);
