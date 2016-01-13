d6910404e207b1121e551bb6c9062c804c53fca0
Brigette Eckert <brigette.eckert@live.com>
2016-01-12 21:51:20 -0800

    added add number function, clear all and clear screen fucntions

--- 02-ziplines/Basic-Front-End-Dev-Projects/04-Calculator/js/app.js
+++ 02-ziplines/Basic-Front-End-Dev-Projects/04-Calculator/js/app.js
@@ -1,26 +1,39 @@
 //store entry until = button is pressed
 //on click add the proper number or fuction
-var buildString = "Hello World";
+//convert buildString to Number before using operators 
+var buildString = "";
+var runningTotal = ""; 
+//running total will hold the whole nummber until presssing equal
 
 //set display up
 $("#display").html(buildString);
 
-
-function numClick(num) {
-      buildString += num
-      console.log("display should be" + buildString)
-}
-//click 1 
-
-$("#1").click(function(){
-  numClick(1)
-  console.log("click")
+//Number Buttons
+$(".num").click(function(e){
+  buildString += e.target.innerHTML;
+  $("#display").html(buildString);
+  
 });
 
 
-//click ac to clear all
-//
-//build_string = "";
+
+
+//Clear Buttons
+
+$("#all_clear").click(function(){
+  buildString = "";
+  $("#display").html(buildString);
+  console.log("all clear")
+});
+
+$("#clear_screen").click(function(){
+  $("#display").html("");
+  console.log("screen cleared");
+  console.log(buildString);
+});
+
+
+
 
 
 //click ce to clear current screen but not build string
