990b55bbd3290e9ed7166812baffe827c1dc46ba
Brigette Eckert <brigette.eckert@live.com>
2016-01-17 23:00:49 -0800

    started on local weather project

--- 02-ziplines/Intermediate-Front-End-Dev-Projects/01-Weather-App/js/app.js
+++
@@ -1,9 +0,0 @@
-// find weather api
-
-
-// change backgroubd based on weather 
-
-// hit enter to submit as well as pushing button
-// tab to get to next piece of form 
-
-// find intnial background
