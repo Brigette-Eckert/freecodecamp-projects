8442b8fe56d5ff857e0738342321776d08604386
Brigette Eckert <brigette.eckert@live.com>
2016-01-30 20:22:27 -0800

    added favicon & got all data displaying on weather app

--- 02-ziplines/Intermediate-Front-End-Dev-Projects/01-Weather-App/js/app.js
+++ 02-ziplines/Intermediate-Front-End-Dev-Projects/01-Weather-App/js/app.js
@@ -1,9 +1,4 @@
-//http://openweathermap.org/current#geo
 
-var key ="e4e1425994f5fa474cdd4b36231633c9";
-// var lat = "";
-// var lon = "";
-// var crd = "";
 //get location
 function success(pos) {
 	  var crd = pos.coords;
@@ -15,7 +10,19 @@ function success(pos) {
 
 	  //Weather API Call
 		$.getJSON("http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=e4e1425994f5fa474cdd4b36231633c9", function(data){
-	  	console.log("success");
+	  	console.log(data);
+	  	var tempK = (data.main.temp).toFixed(1);
+	  	var tempC = ((data.main.temp)-273.15).toFixed(1);
+	  	var tempF = ((data.main.temp)*(9/5)-459.67).toFixed(1);
+	  
+	  	$('#tempDisplay').html(tempC+"°");
+	  	//+"<img src="+data.weather[0].icon+">"
+	  	$('#location').html(data.name +", "+ data.sys.country);
+	  	$('#condition').html("<p>"+data.weather[0].main+"</p>"+"<p>"+data.weather[0].description+"</p>");
+	  	$('#wind').html("<p> Wind Speed: " + data.wind.speed +"</p>");
+	  	$('#humid').html("<p> Humidity: " + data.main.humidity +"<p>");
+	  	$('#sunTime').html("<p id='sunrise'>Sunrise: " + data.sys.sunrise + "</p>"+"<p id='sunset'>Sunset: " +data.sys.sunrise + "</p>");
+	  	
 		});
 
 	};
@@ -25,7 +32,7 @@ function success(pos) {
 
 navigator.geolocation.getCurrentPosition(success, error);
 
-
+//cover to celicus minus -273.15 degrees
 
 
 //Display Conditons  
