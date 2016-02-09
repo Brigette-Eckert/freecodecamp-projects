f1364b8174c2aadb6c5b1fc150d49298fd5339a7
Brigette Eckert <brigette.eckert@live.com>
2016-02-08 22:41:09 -0800

    added angular js module and api call

--- 02-ziplines/Intermediate-Front-End-Dev-Projects/02-Camper-News/js/app.js
+++ 02-ziplines/Intermediate-Front-End-Dev-Projects/02-Camper-News/js/app.js
@@ -1,1 +1,8 @@
-// API End point http://www.freecodecamp.com/news/hot
+//need controller 
+angular.module('camperNewsApp', [])
+
+$http.get('http://www.freecodecamp.com/news/hot').success(function(data){
+	console.log(data);
+}).error(function(err){
+	console.log(err);
+})
