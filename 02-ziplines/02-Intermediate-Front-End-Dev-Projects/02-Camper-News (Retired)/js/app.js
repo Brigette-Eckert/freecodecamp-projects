a273ef9d5c7976ce6503a2b1a44b8cf7984624e8
Brigette Eckert <brigette.eckert@live.com>
2016-02-13 20:21:47 -0800

    added default fcc icon for stories w/o an image

--- 02-ziplines/02-Intermediate-Front-End-Dev-Projects/02-Camper-News (Retired)/js/app.js
+++ 02-ziplines/02-Intermediate-Front-End-Dev-Projects/02-Camper-News (Retired)/js/app.js
@@ -4,6 +4,11 @@ angular.module('camperNewsApp', [])
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http){
 	$http.get('http://www.freecodecamp.com/news/hot').success(function(data){
 		$scope.stories = data;
+		for(i = 0; i < $scope.stories.length; i++){
+			if($scope.stories[i].image == ""){
+				$scope.stories[i].image  ="https://avatars2.githubusercontent.com/u/9892522?v=3&s=400";
+			}
+		 }
 		console.log(data);
 	}).error(function(err){
 		console.log(err);
