239796bd667dfb4c24dcc8f4f2e231c3ebc60aa0
Brigette Eckert <brigette.eckert@live.com>
2016-02-20 11:56:48 -0800

    got api call working on twitch zipline

--- 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
+++ 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
@@ -1,11 +1,16 @@
 angular.module('twitchStatusApp', [])
 	
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http){
+	$scope.user = "";
 	$scope.twitchUsers = ["freecodecamp", "EtchTheSketch", "SoXvicious", "Dexteritybonus", "patrickrothfuss", "FeliciaDay", "ShaBooZey", "Monstercat", "TotalBiscuit", "Crendor"];
-	$http.get(url).success(function(data){
+	for(i=0; i < $scope.twitchUsers.length; i++){
+		$scope.user = $scope.twitchUsers[i];
+		console.log("user " + $scope.user);
+		$http.get("https://api.twitch.tv/kraken/streams/" + $scope.user).success(function(data){
 		console.log(data)
-
-	})
+		})
+	};
+	
 }])
 
 
