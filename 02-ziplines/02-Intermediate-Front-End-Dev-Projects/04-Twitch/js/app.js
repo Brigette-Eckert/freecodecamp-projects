32b1c7831dcb3adddf333b7afc44e8af8b880ff4
Brigette Eckert <brigette.eckert@live.com>
2016-02-20 13:56:56 -0800

    playing with second url request to get data regardless of online status

--- 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
+++ 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
@@ -2,16 +2,21 @@ angular.module('twitchStatusApp', [])
 	
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.user = ""
-	$scope.twitchUsers = ["freecodecamp", "EtchTheSketch", "SoXvicious", "Dexteritybonus", "patrickrothfuss", "FeliciaDay", "ShaBooZey", "Monstercat", "TotalBiscuit", "Crendor"];
+	$scope.twitchUsers = ["freecodecamp", "EtchTheSketch", "SoXvicious", "Dexteritybonus", "patrickrothfuss", "FeliciaDay", "ShaBooZey", "Monstercat", "TotalBiscuit", "Crendor", "brunofin", "comster404"];
 	$scope.userData =[];
 	for(i=0; i < $scope.twitchUsers.length; i++){
 		$scope.user = $scope.twitchUsers[i];
 		// console.log("user " + $scope.user);
 		$http.get("https://api.twitch.tv/kraken/streams/" + $scope.user).success(function(data){
-			console.log(data)
-			$scope.username = data._links.self.split('/').pop();
-			console.log("username " + $scope.username);
-			$scope.userData.push(data);
+			console.log(data);
+			if(stream==null){
+			$http.get(data._links.self).success(function(info){
+				console.log(info);
+				$scope.userData.push(info);
+				})
+			}
+			// data.username = data._links.self.split('/').pop();
+			// console.log("username " + $scope.username);
 			console.log($scope.userData);
 		})
 	};
@@ -20,11 +25,10 @@ angular.module('twitchStatusApp', [])
 
 
 
-//add user name 
-
-//add online vs offline status 
-// to get user name - spilt channel url at / - pop username
 //add avatar 
+
 
 //add data scope for offline vs offline of where to get in 
 //class offline or online for sass to make color as same as fliter 
+
+// add info for error and show as account closed
