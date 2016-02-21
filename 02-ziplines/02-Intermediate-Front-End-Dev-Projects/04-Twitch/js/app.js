e81f67a2ea37e0bb2bf6532aa3d5bbbb8ae1104a
Brigette Eckert <brigette.eckert@live.com>
2016-02-21 14:29:04 -0800

    playing with second url request to get data regardless of online status

--- 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
+++ 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
@@ -2,23 +2,30 @@ angular.module('twitchStatusApp', [])
 	
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.user = ""
-	$scope.twitchUsers = ["freecodecamp", "EtchTheSketch", "SoXvicious", "Dexteritybonus", "patrickrothfuss", "FeliciaDay", "ShaBooZey", "Monstercat", "TotalBiscuit", "Crendor", "brunofin", "comster404"];
+	$scope.twitchUsers = ["freecodecamp", "EtchTheSketch", "SoXvicious", "Dexteritybonus", "patrickrothfuss", "FeliciaDay", "ShaBooZey", "Monstercat", "TotalBiscuit", "Crendor"];
 	$scope.userData =[];
 	for(i=0; i < $scope.twitchUsers.length; i++){
 		$scope.user = $scope.twitchUsers[i];
 		// console.log("user " + $scope.user);
 		$http.get("https://api.twitch.tv/kraken/streams/" + $scope.user).success(function(data){
-			console.log(data);
-			if(stream==null){
+			// console.log("og data " + data);
+			console.log("streaming status " + data.stream)
+			if(data.stream == "null"){
 			$http.get(data._links.self).success(function(info){
-				console.log(info);
-				$scope.userData.push(info);
+				console.log("offline");
+				// $scope.userData.push(info);
 				})
+			} else if(data.stream != "null") {
+				$scope.channel = data.stream.channel;
+				$scope.name = $scope.channel.display_name;
+				$scope.game = $scope.channel.game;
+				$scope.avatar = $scope.channel.logo;
+				$scope.userInfo = {name:'"' + scope.name + '"', game: '"' + scope.game + '"', avatar: '"' + scope.avatar + '"'}
+			    $scope.userData.push($scope.userInfo);			
 			}
-			// data.username = data._links.self.split('/').pop();
-			// console.log("username " + $scope.username);
 			console.log($scope.userData);
 		})
+		
 	};
 	
 }])
@@ -32,3 +39,4 @@ angular.module('twitchStatusApp', [])
 //class offline or online for sass to make color as same as fliter 
 
 // add info for error and show as account closed
+//"brunofin", "comster404" closed accounts
