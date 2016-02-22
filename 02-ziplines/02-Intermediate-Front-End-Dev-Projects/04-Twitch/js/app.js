b722b8e7cfc141fd3be9910a02fcd9ac62d7d2e6
Brigette Eckert <brigette.eckert@live.com>
2016-02-21 18:07:49 -0800

    added styles to twitch project

--- 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
+++ 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
@@ -1,13 +1,12 @@
 angular.module('twitchStatusApp', [])
 	
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http){
-	$scope.user = ""
-	$scope.twitchUsers = ["freecodecamp", "EtchTheSketch", "SoXvicious", "Dexteritybonus", "patrickrothfuss", "FeliciaDay", "ShaBooZey", "Monstercat", "TotalBiscuit", "Crendor"];
+	$scope.twitchUsers = ["freecodecamp", "EtchTheSketch", "SoXvicious", "Dexteritybonus", "patrickrothfuss", "FeliciaDay", "ShaBooZey", "Monstercat", "TotalBiscuit", "Crendor", "comster404", "brunofin"];
 	$scope.userData =[];
 
 	for(i=0; i < $scope.twitchUsers.length; i++){
 		$scope.user = $scope.twitchUsers[i];
-		$http.get("https://api.twitch.tv/kraken/streams/" + $scope.user).success(function(data) {
+		$http.get("https://api.twitch.tv/kraken/streams/" + $scope.user).success(function(data){
 			if(data.stream == null){
 				// console.log("offline")
 				$http.get(data._links.channel).success(function(info){
@@ -28,6 +27,15 @@ angular.module('twitchStatusApp', [])
 			    $scope.userData.push($scope.userInfo);		
 			    console.log($scope.userData);	
 			}
+		}).error(function(error){
+			console.log(error)
+			$scope.name = error.message.split("'")[1];
+			console.log("name " + $scope.name)
+			$scope.avatar = "img/twitch.png";
+			$scope.game = "No Account";
+			$scope.userInfo = {name: $scope.name , game: $scope.game, avatar: $scope.avatar}
+			$scope.userData.push($scope.userInfo);	
+			console.log($scope.userData);	
 		})
 
 	};
@@ -35,11 +43,9 @@ angular.module('twitchStatusApp', [])
 
 
 
-//add avatar 
 
 
-//add data scope for offline vs offline of where to get in 
+//use ng-show to show/hide based on online status
 //class offline or online for sass to make color as same as fliter 
 
-// add info for error and show as account closed
-//"brunofin", "comster404" closed accounts
+//sort userData 
