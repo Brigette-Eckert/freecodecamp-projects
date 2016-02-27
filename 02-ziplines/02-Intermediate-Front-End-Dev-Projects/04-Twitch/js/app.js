c2c7f7e8eecee51eaeb86bcbb4aadfb636e62d3d
Brigette Eckert <brigette.eckert@live.com>
2016-02-27 12:22:48 -0800

    changed  to var where  not needed in twtich project

--- 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
+++ 02-ziplines/02-Intermediate-Front-End-Dev-Projects/04-Twitch/js/app.js
@@ -7,6 +7,10 @@ angular.module('twitchStatusApp', [])
 	$scope.userView =[];
 
 	//set up function that fliters array that goes into ng-view 
+	//write displayAll()
+	//write displayOnline()
+	//write displayOffline()
+
 
 	for(i=0; i < $scope.twitchUsers.length; i++){
 		$scope.user = $scope.twitchUsers[i];
@@ -15,33 +19,33 @@ angular.module('twitchStatusApp', [])
 				// console.log("offline")
 				$http.get(data._links.channel).success(function(info){
 					// console.log(info);
-					$scope.name = info.display_name;
-					$scope.avatar = info.logo;
-					$scope.game = "Offline";
-					$scope.status = "Offline";
-					$scope.userInfo = {name: $scope.name , game: $scope.game, avatar: $scope.avatar, status: $scope.status}
-					$scope.userData.push($scope.userInfo);		
+					var name = info.display_name;
+					var avatar = info.logo;
+					var game = "Offline";
+					var status = "Offline";
+					var userInfo = {name: name , game: game, avatar: avatar, status: status}
+					$scope.userData.push(userInfo);		
 				    console.log($scope.userData);
 				})
 			} else {
-				$scope.channel = data.stream.channel;
-				$scope.name = $scope.channel.display_name;
-				$scope.game = $scope.channel.game;
-				$scope.avatar = $scope.channel.logo;
-				$scope.status = "Online";
-				$scope.userInfo = {name: $scope.name , game: $scope.game, avatar: $scope.avatar, status: $scope.status}
-			    $scope.userData.push($scope.userInfo);	
+				var channel = data.stream.channel;
+				var name = channel.display_name;
+				var game = channel.game;
+				var avatar = channel.logo;
+				var status = "Online";
+				var userInfo = {name: name , game: game, avatar: avatar, status: status}
+			    $scope.userData.push(userInfo);	
 			    console.log($scope.userData);
 			}
 		}).error(function(error){
 			console.log(error)
-			$scope.name = error.message.split("'")[1];
-			console.log("name " + $scope.name)
-			$scope.avatar = "img/notwitch.png";
-			$scope.game = "No Account";
-			$scope.status = "Offline";
-			$scope.userInfo = {name: $scope.name , game: $scope.game, avatar: $scope.avatar, status: $scope.status}
-			$scope.userData.push($scope.userInfo);	
+			var name = error.message.split("'")[1];
+			console.log("name " + name)
+			var avatar = "img/notwitch.png";
+			var game = "No Account";
+			var status = "Offline";
+			var userInfo = {name: name , game: game, avatar: avatar, status: status}
+			$scope.userData.push(userInfo);	
 			console.log($scope.userData);	
 		})
 
