0dcb282a31ca50b14f1eebfb808eebd9eb083761
Brigette Eckert <brigette.eckert@live.com>
2016-03-01 20:37:21 -0800

    solved smallest common mutliple bonfire started on drop it bonfire

--- 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-15-Smallest-common-mutliple.js
+++ 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-15-Smallest-common-mutliple.js
@@ -1,43 +1,34 @@
+smallestCommons([1,13]);
+
+
 //Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
 //The range will be an array of two numbers that will not necessarily be in numerical order.
 //e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3 that is evenly divisible by all numbers between 1 and 3.
 
 function smallestCommons(arr) {
-    var nums =[];
-    var scm = 0;
-   
 
+  var nums = arr.sort(function(a, b) { return a - b });
+  var product = Array(nums[1] - nums[0]).join(nums[0] + ",").split(",").reduce(function(a, b, i) {
+    return a * (b + 1);
+  })
+  for(i = 2; i < product; i++) {
+    var isCommonMultiple = true;
 
-   //setting lower number to come first so loop to build num array will work properly
-  if(arr[0] <= arr[1]){
-    var num1 = arr[0];
-    var num2 = arr[1];
-  } else {
-    var num1 = arr[1];
-    var num2 = arr[0];
+    for(j=nums[0]; j <= nums[1]; j++) {
+      if(i % j !== 0) {
+        isCommonMultiple = false;
+        break;
+      } 
+    }
+    if(isCommonMultiple) {
+      console.log(i)
+      return i;
+    }
   }
-
-  //creating number array of all numbers that need to be checked to find scm  
-  for(i = num1; i <= num2; i++){
-    nums.push(i)
-  }
-  var cm = nums.reduce(function(a,b){return a*b;});
-  
-  // while(scm === 0){
-  //   console.log("checking for common multiples")
-
-  // }
-  for(i=0; i <= cm; i++){
-    console.log(nums[i]%i);
-  }
-  console.log(cm)
-  console.log(nums)
-  
-  return scm;
-
 }
 
 
 smallestCommons([1,5]);
 smallestCommons([5,1]);
-// smallestCommons([1,13]);
+smallestCommons([1,13]);
+smallestCommons([1,13]);
---
+++ 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-17-Drop-it.js
@@ -0,0 +1,20 @@
+// Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+// Return the rest of the array, otherwise return an empty array.
+
+function drop(arr, func) {
+  for(i = 0; i < arr.length; i++){
+  	console.log([arr[i]]);
+  	if(func(arr[i]) == false){
+  		console.log("false")
+  	} else {
+  		console.log("true")
+  	}
+  }
+  return arr;
+}
+
+drop([1, 2, 3], function(n) {return n < 3; });
+// drop([1, 2, 3, 4], function(n) {return n >= 3;});
+// drop([0, 1, 0, 1], function(n) {return n === 1;});
+// drop([1, 2, 3, 4], function(n) {return n > 5;});
+// drop([1, 2, 3, 9, 2], function(n) {return n > 2;});
