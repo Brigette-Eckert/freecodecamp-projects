7a287b08143c98edb390e98340437df9a4601b0a
Brigette Eckert <brigette.eckert@live.com>
2016-03-01 22:06:14 -0800

    finished intermediate bonfire17 Drop It

--- 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-17-Drop-it.js
+++ 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-17-Drop-it.js
@@ -2,19 +2,24 @@
 // Return the rest of the array, otherwise return an empty array.
 
 function drop(arr, func) {
+var slice = []; 
   for(i = 0; i < arr.length; i++){
-  	console.log([arr[i]]);
-  	if(func(arr[i]) == false){
-  		console.log("false")
+  	console.log(arr[i]);
+  	if(func(arr[i]) === true){
+  		console.log("true");
+  	    slice = arr.slice(i);
+  		break;
   	} else {
-  		console.log("true")
+  	 console.log("false");
   	}
   }
-  return arr;
+  console.log(slice)
+  return slice;
 }
 
 drop([1, 2, 3], function(n) {return n < 3; });
-// drop([1, 2, 3, 4], function(n) {return n >= 3;});
-// drop([0, 1, 0, 1], function(n) {return n === 1;});
-// drop([1, 2, 3, 4], function(n) {return n > 5;});
-// drop([1, 2, 3, 9, 2], function(n) {return n > 2;});
+drop([1, 2, 3, 4], function(n) {return n >= 3;});
+drop([0, 1, 0, 1], function(n) {return n === 1;});
+drop([1, 2, 3, 4], function(n) {return n > 5;});
+drop([1, 2, 3, 9, 2], function(n) {return n > 2;});
+drop([1, 2, 3], function(n) {return n > 0;});
