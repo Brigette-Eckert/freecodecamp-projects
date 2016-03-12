72641e31f3edd3d12674d974f7465d1881630cd8
Brigette Eckert <brigette.eckert@live.com>
2016-03-11 20:14:59 -0800

    finished binary agents bonfire

--- 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-19-Binary-agents.js
+++ 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-19-Binary-agents.js
@@ -3,12 +3,17 @@
 
 function binaryAgent(str) {
 	var biArr = str.split(" ");
+	 var secretMessage = "";
 	for(i=0; i < biArr.length; i++){
-		console.log(biArr[i]);
-		console.log(String.fromCharCode(biArr[i]));
+		// console.log(biArr[i]);
+		var unicode = parseInt(biArr[i], 2);
+		// console.log(unicode);
+		var character = String.fromCharCode(unicode);
+		secretMessage += character;  
 
 	}
-  return str;
+  console.log(secretMessage);
+  return secretMessage;
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
