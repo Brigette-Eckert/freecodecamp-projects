503e3a89db59ad6541793373885156bd6a82fc37
Brigette Eckert <brigette.eckert@live.com>
2016-03-31 22:34:30 -0700

    solved intermediate bonfire12 spinal tap case

--- 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-12-Spinal-tap.js
+++ 01-bonfires/02-Intermediate-Algoritm-Scripting/Bonfire-12-Spinal-tap.js
@@ -2,9 +2,19 @@
 
 
 function spinalCase(str) {
-	var casedStr = str.replace(/[\s]/g, "-");
+	//check for an upper letter following a lower case letter
+	var spacedStr = str.replace(/([a-z])([A-Z])/g, "$1 $2");
+	//check for anything other than alhphanumeric characters and replace with -
+	var spinalStr = spacedStr.replace(/[\W_]/g, "-");
+		//check for capitlized letters and convert to lower case [A-Z]
+	var casedStr = spinalStr.replace(/[A-Z]/g, function(x){
+		return x.toLowerCase();
+		});
+
 	console.log(casedStr);
   	return casedStr;
 }
 
 spinalCase('This Is Spinal Tap');
+spinalCase("The_Andy_Griffin_Show");
+spinalCase("thisIsSpinalTap");
