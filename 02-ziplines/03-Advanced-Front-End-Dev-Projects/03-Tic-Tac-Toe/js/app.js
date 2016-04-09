789bbccf6f392559f408cbb6d1716c43f1b2a0bc
Brigette Eckert <brigette.eckert@live.com>
2016-04-09 11:57:18 -0700

    added some sass and js notes to tic tac toe

--- 02-ziplines/03-Advanced-Front-End-Dev-Projects/03-Tic-Tac-Toe/js/app.js
+++ 02-ziplines/03-Advanced-Front-End-Dev-Projects/03-Tic-Tac-Toe/js/app.js
@@ -1,5 +1,23 @@
-//set win condtions
+// have player choose x or o- make AI chose other
 
+
+//set win condtions
+//1,2,3 
+//4,5,6
+//7,8,9
+//1,4,7
+//2,5,8
+//3.6.9
+//1,5,9
+//3,5,7
+
+var gameboard = [{1: "empty"}, {2: "empty"}, {3: "empty"}, {4: "empty"}, {5: "empty"}, {6: 
+"empty"}, {7: "empty"}, {8: "empty"}, {9: "empty"}];
 //set state of each square: empty, controlled by x or controlled by o
+//set up turns- player goes first
+
+//on click- fill in square clicked and then have comuter fill in another square
 
 //have computer pick num1-9 on turn if not occupied or keep track of avalbile squares with array and choose from that
+
+//draw a line bewtween spaces when win condtion is met 
