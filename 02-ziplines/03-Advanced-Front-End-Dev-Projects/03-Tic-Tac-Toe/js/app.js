96a31bd7cb1f094b14b765744b3a12a6b1861c51
Brigette Eckert <brigette.eckert@live.com>
2016-05-03 21:06:22 -0700

    finished setting up win conditions

--- 02-ziplines/03-Advanced-Front-End-Dev-Projects/03-Tic-Tac-Toe/js/app.js
+++ 02-ziplines/03-Advanced-Front-End-Dev-Projects/03-Tic-Tac-Toe/js/app.js
@@ -3,19 +3,15 @@ var player = "";
 var ai = "";
 var winner = "none";
 var turn = 1;
-//set state of each square: intially empty 
-var gameboard = [{1: "empty"}, {2: "empty"}, {3: "empty"}, {4: "empty"}, {5: "empty"}, {6: 
-"empty"}, {7: "empty"}, {8: "empty"}, {9: "empty"}];
-//manipulate dom - to display intinal choose X or O, show gameboard during play, and then play again. 
-
-
-//set win condtions
-
-//1,5,9
-//3,5,7
 
 player = "X"; 
 ai = "O";
+//set state of each square: intially empty 
+var gameboard = ["X", "X", "X", "", "", "", "", "", ""];
+;
+//manipulate dom - to display intinal choose X or O, show gameboard during play, and then play again. 
+
+console.log(gameboard[0]);
 
 
 for(i=0; i < 10; i++){
@@ -26,6 +22,7 @@ for(i=0; i < 10; i++){
 	} else {
 		// console.log("players's turn")
 	}
+	checkForWinner();
 	turn ++; 
 }
 
@@ -37,35 +34,47 @@ for(i=0; i < 10; i++){
 
 
 
-
-//check for win conditions 
-if(gameboard[0] === player && gameboard[1] === player && gameboard[2] === player){
-	winner = "player";
-} else if(gameboard[0] === ai && gameboard[1] === ai && gameboard[2] === ai){
-	winner = "computer";
-}else if(gameboard[5] === player && gameboard[6] === player && gameboard[7] === player){
-	winner = "player";
-}else if(gameboard[5] === ai && gameboard[6] === ai && gameboard[7] === ai){
-	winner = "computer";
-} else if(gameboard[8] === player && gameboard[9] === player && gameboard[10] === player){
-	console.log("player wins");
-	winner = "player";
-}else if(gameboard[8] === ai && gameboard[9] === ai && gameboard[10] === ai){
-	winner = "computer";
-} else if(gameboard[3] === player && gameboard[6] === player && gameboard[9] === player){
-	console.log("player wins");
-	winner = "player";
-}else if(gameboard[3] === ai && gameboard[6] ===ai && gameboard[9] ===ai){
-	winner = "computer";
-
-} else if(gameboard[4] === player && gameboard[7] === player && gameboard[10] === player){
-	console.log("player wins");
-	winner = "player";
-}else if(gameboard[4] === ai && gameboard[7] ===ai && gameboard[10] ===ai){
-	winner = "computer";
+function checkForWinner(){
+	//check for win conditions 
+//horizontal win
+	if(gameboard[0] === player && gameboard[1] === player && gameboard[2] === player){
+		winner = "player";
+	} else if(gameboard[0] === ai && gameboard[1] === ai && gameboard[2] === ai){
+		winner = "computer";
+	}else if(gameboard[5] === player && gameboard[6] === player && gameboard[7] === player){
+		winner = "player";
+	}else if(gameboard[5] === ai && gameboard[6] === ai && gameboard[7] === ai){
+		winner = "computer";
+	} else if(gameboard[8] === player && gameboard[9] === player && gameboard[10] === player){
+		winner = "player";
+	}else if(gameboard[8] === ai && gameboard[9] === ai && gameboard[10] === ai){
+		winner = "computer";
+	//vertical win
+	} else if(gameboard[2] === player && gameboard[5] === player && gameboard[8] === player){
+		winner = "player";
+	}else if(gameboard[2] === ai && gameboard[5] ===ai && gameboard[8] ===ai){
+		winner = "computer";
+	} else if(gameboard[3] === player && gameboard[6] === player && gameboard[9] === player){
+		winner = "player";
+	}else if(gameboard[3] === ai && gameboard[6] ===ai && gameboard[9] ===ai){
+		winner = "computer";
+	} else if(gameboard[4] === player && gameboard[7] === player && gameboard[10] === player){
+		winner = "player";
+	}else if(gameboard[4] === ai && gameboard[7] ===ai && gameboard[10] ===ai){
+		winner = "computer";
+	}
+	//diagonal win
+	else if(gameboard[2] === player && gameboard[6] === player && gameboard[10] === player){
+		winner = "player";
+	}else if(gameboard[2] === ai && gameboard[6] ===ai && gameboard[10] ===ai){
+		winner = "computer";
+	} else if(gameboard[4] === player && gameboard[6] === player && gameboard[8] === player){
+		winner = "player";
+	}else if(gameboard[4] === ai && gameboard[6] ===ai && gameboard[7] ===ai){
+		winner = "computer";
+	}
+	console.log(winner);
 }
-
-console.log(gameboard[0]);
 
 
 
