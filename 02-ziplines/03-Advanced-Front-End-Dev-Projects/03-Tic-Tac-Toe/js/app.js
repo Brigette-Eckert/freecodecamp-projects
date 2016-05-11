eeb263737f15746a71d16bd970b481ccef89bae6
Brigette Eckert <brigette.eckert@live.com>
2016-05-10 21:42:27 -0700

    finished tic tac toe

--- 02-ziplines/03-Advanced-Front-End-Dev-Projects/03-Tic-Tac-Toe/js/app.js
+++ 02-ziplines/03-Advanced-Front-End-Dev-Projects/03-Tic-Tac-Toe/js/app.js
@@ -1,109 +1,127 @@
-var player = "O"; 
-var ai = "X";
+var player = "X"; 
+var ai = "O";
 var winner, gameboard, playing;
-// have player choose x or o- make AI chose other
 
-//find way to assign array to match sq so sq+num = gameboard[num]
-//manipulate dom - to display intinal choose X or O, show gameboard during play, and then play again. 
+//toggle player symbol based on button clicked
+$("#symbol-X").click(function() {
+  player = "X";
+  ai = "O";
+  $("#symbol-O").removeClass("glow");
+  console.log("player: " + player);
+  reset();
+});
 
-//players = {
-//	"X": "Player",
-//  "O": "Computer"
-//}
-// e.g. players["X"] => "Player"
+$("#symbol-O").click(function() {
+  player = "O";
+  ai = "X";
+  $("#symbol-X").removeClass("glow");
+  console.log("player: " + player);
+  reset();
+});
 
-
-function reset(){
-	setTimeout(function() {
-		start();
-		$('.square').removeClass("glow");
-	 	console.log("game reset");
-	 	$('.square').html("");
-	}, 3000);
+function reset() {
+  setTimeout(function() {
+    start();
+    $('.square').removeClass("glow");
+    console.log("game reset");
+    $('.square').html("");
+  }, 1500);
 };
 
 function start() {
-	$('#symbol-' + player).addClass('glow');
-	gameboard = ["", "", "", "", "", "", "", "", ""];
-	playing = true;
-	winner = "none";
+  $('#symbol-' + player).addClass('glow');
+  gameboard = ["", "", "", "", "", "", "", "", ""];
+  playing = true;
+  winner = "none";
 
 }
 
-function aiTurn(){
-	console.log("computer's turn");
-	//have computer pick num0-8 on turn if not occupied or keep track of available squares with array and choose from that
-	var aiPick= (Math.floor(Math.random() * 9));
-	var aiSelector = "#sq" + aiPick;
-	// console.log(aiPick);
-	if(gameboard[aiPick] == ""){
-		gameboard[aiPick] = ai;
-		console.log(gameboard);
-		$(aiSelector).html(ai);
-			//find way to reroll if AI selets already taken spot
-		}; 
+function getEmptySpaces(board) {
+  return board.reduce(function(results, square,index) {
+    if(square !== "") {
+      return results;
+    } else {
+      return results.concat(index);
+    }
+  }, []);
+}
+
+function aiTurn() {
+  console.log("computer's turn");
+  var available = getEmptySpaces(gameboard);
+  console.log(available);
+  //temporary stop if tie game
+  if(available.length === 0) {
+    return;
+  }
+  var aiIndex = available[Math.floor(Math.random() * available.length)];
+  console.log(aiIndex);
+  gameboard[aiIndex] = ai;
+  $("#sq" + aiIndex).html(ai);
 };
 
 
-function checkForWinner(board){
-	// Check for win conditions 
-	var winCon = [
-		// Horizontal
-		[0, 1, 2], 
-		[3, 4, 5],
-		[6, 7, 8],
-		// Vertical
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		// Diagonal
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for(var i=0; i<winCon.length; i++) {
-		var cond = winCon[i];
-		// If all three are equal and not blank
-		if(board[cond[0]] !== ""  && board[cond[0]] === board[cond[1]] && board[cond[1]] === board[cond[2]]) {
-			winner = board[cond[0]];
-			playing = false;
-			$('#sq'+ cond[0]).addClass('glow');
-			$('#sq'+ cond[1]).addClass('glow');
-			$('#sq'+ cond[2]).addClass('glow');
-			console.log(winner + " Wins!");
-			reset();
-			break;
-		}
-	}
+function checkForWinner(board) {
+  // Check for win conditions 
+  var winCon = [
+    // Horizontal
+    [0, 1, 2], 
+    [3, 4, 5],
+    [6, 7, 8],
+    // Vertical
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    // Diagonal
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for(var i=0; i<winCon.length; i++) {
+    var cond = winCon[i];
+    // If all three are equal and not blank
+    if(board[cond[0]] !== ""  && board[cond[0]] === board[cond[1]] && board[cond[1]] === board[cond[2]]) {
+      winner = board[cond[0]];
+      playing = false;
+      $('#sq'+ cond[0]).addClass('glow');
+      $('#sq'+ cond[1]).addClass('glow');
+      $('#sq'+ cond[2]).addClass('glow');
+      console.log(winner + " Wins!");
+      reset();
+      break;
+    }
+  }
+  if(getEmptySpaces(gameboard).length === 0 && winner === "none") {
+    console.log("Game tie!");
+    reset();
+  }
 };
 
 
-$("#gameboard").click(function(e){
-	//end game when winner delcared 
-	if(!playing) return;
-	console.log("players's turn");
-	var playerPick = (e.target.id).slice(2);
-	console.log(playerPick);
-	var playerSelector = "#sq" + playerPick;
-	if(gameboard[playerPick] != ""){
-	   //player doesn't lose turn if invalid entry 
-		return;
-	};
-	if(gameboard[playerPick] == "") {
-		gameboard[playerPick] = player;
-		console.log(gameboard);
-		$(playerSelector).html(player);
-	}; 
+$("#gameboard").click(function(e) {
+  //end game when winner delcared 
+  if(!playing) return;
+  console.log("players's turn");
+  var playerPick = (e.target.id).slice(2);
+  console.log(playerPick);
+  var playerSelector = "#sq" + playerPick;
+  if(gameboard[playerPick] != "") {
+     //player doesn't lose turn if invalid entry 
+    return;
+  };
+  if(gameboard[playerPick] == "") {
+    gameboard[playerPick] = player;
+    console.log(gameboard);
+    $(playerSelector).html(player);
+  }; 
 
-	checkForWinner(gameboard); 
-	if(playing){
-		aiTurn(); 
-		checkForWinner(gameboard);
-	}
+  checkForWinner(gameboard); 
+  if(playing) {
+    aiTurn(); 
+    checkForWinner(gameboard);
+  }
 });
 
 
 
 start();
-    	
-
-//draw a line bewtween spaces when win condtion is met or sass animation - glow/flash etc 
+      
