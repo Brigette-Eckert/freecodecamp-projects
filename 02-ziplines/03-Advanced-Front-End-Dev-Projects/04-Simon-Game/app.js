dd0fd7ebb57c5e1792dff428faa73d4c1c25d921
Brigette Eckert <brigette.eckert@live.com>
2016-05-11 00:54:30 -0700

    set up basics for simon to use angular1

--- 02-ziplines/03-Advanced-Front-End-Dev-Projects/04-Simon-Game/app.js
+++ 02-ziplines/03-Advanced-Front-End-Dev-Projects/04-Simon-Game/app.js
@@ -1,39 +1,50 @@
-var count; 
-var moves;
-var mode;
+angular.module('simonApp', [])
+
+//use ES6 and Babel
+
+.controller('.gameCtrl',['$scope', function($scope){
+
+		//starts the game 
+	function start(){
+
+	}
 
 
-//starts the game 
-function start(){
+	function reset(){
+		count = 0; 
+		moves = [];
+		mode = "casual"
+	}
 
-}
+	function strictOn(){
+		count = 0; 
+		moves = [];
+		mode = "strict"
+	}
 
+	function turn(){
+		//set each square 1-4, have computer pick random number and add to array of moves. 
+		//green = 1; 
+		//red =2;
+		//yellow =3; 
+		//blue =4; 
 
-function reset(){
-	count = 0; 
-	moves = [];
-	mode = "casual"
-}
+		//loop over array to display moves 
 
-function strictOn(){
-	count = 0; 
-	moves = [];
-	mode = "strict"
-}
+		//store user moves in array- check for match- if match add +1 to count, if count == 20 then user wins
 
-function turn(){
-	//set each square 1-4, have computer pick random number and add to array of moves. 
-	//green = 1; 
-	//red =2;
-	//yellow =3; 
-	//blue =4; 
-
-	//loop over array to display moves 
-
-	//store user moves in array- check for match- if match add +1 to count, if count == 20 then user wins
-
-	//have user repeat, if user fails -repeat or if in strict mode, reset to one
+		//have user repeat, if user fails -repeat or if in strict mode, reset to one
 
 
+	function win(){
+		//display win message when game is won
+	}
 
-}
+	}
+
+
+}]);
+// var count; 
+// var moves;
+// var mode;
+
