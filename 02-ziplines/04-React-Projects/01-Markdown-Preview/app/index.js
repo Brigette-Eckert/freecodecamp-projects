1593287fda735fae863da435ceabb36b486b212b
Brigette Eckert <brigette.eckert@live.com>
2016-07-02 13:07:30 -0700

    got react text rendering properly in markdown preview project

--- 02-ziplines/04-React-Projects/01-Markdown-Preview/app/index.js
+++ 02-ziplines/04-React-Projects/01-Markdown-Preview/app/index.js
@@ -4,17 +4,23 @@ import ReactDOM from 'react-dom';
 
 console.log(marked('I am using __markdown__.'));
 
+
+
 var HelloMessage = React.createClass({
-    render: function() {
-        return <div>Hello {this.props.name}</div>;
+    render: function () {
+        return <h1>Hello {this.props.message}! </h1>;
     }
 });
+
+ReactDOM.render(<HelloMessage message="World" /> , document.getElementById('output'));
 
 // get data from textbox and pass into MarkDownText function
 //
 // var MarkDownText = React.createClass({
 //     render: function (data) {
-//         return marked(data);
+//         return marked<p> {this.props.fata} </p?;
 //     }
 // })
+
+//ReactDom.render(<MarkDownText data = "#Text" /> document.getElementById('output'));
 
