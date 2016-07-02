96aec1902a41cac295e55f7bfd5c2d659385079f
Brigette Eckert <brigette.eckert@live.com>
2016-07-01 19:53:53 -0700

    fixed git log error

---
+++ 02-ziplines/04-React-Projects/01-Markdown-Preview/app/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// require('./main.scss');
+console.log(marked('I am using __markdown__.'));
+
+var HelloMessage = React.createClass({
+    render: function() {
+        return <div>Hello {this.props.name}</div>;
+    }
+});
+
+// get data from textbox and pass into MarkDownText function
+//
+// var MarkDownText = React.createClass({
+//     render: function (data) {
+//         return marked(data);
+//     }
+// })
+
