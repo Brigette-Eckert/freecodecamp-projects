f275f7b8a5ad7d725112e298f1e6b8bb8f4a7ac6
Brigette Eckert <brigette.eckert@live.com>
2016-10-05 21:35:52 -0700

    Camper Leaderboard: Set up states and buttons to toggle between recent points and all time points

--- 02-ziplines/04-React-Projects/02-Camper-Leaderboard/ReadMe.md
+++ 02-ziplines/04-React-Projects/02-Camper-Leaderboard/ReadMe.md
@@ -5,6 +5,7 @@
  Practiced using JSX
  Practiced using Babel 
  Practiced using Webpack
+ Particed using Bootstrap with SASS
  
 ```
 #User Stories
--- 02-ziplines/04-React-Projects/02-Camper-Leaderboard/app/index.js
+++ 02-ziplines/04-React-Projects/02-Camper-Leaderboard/app/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
+
+//
 require('./stylesheets/main.scss');
 
 //set up components
+// use array map- return jsx
 
 //Leader Board Labels
 var BoardLabels = React.createClass({
@@ -12,52 +15,63 @@ var BoardLabels = React.createClass({
         return(
             <div id="board-labels">
                 <div id="name">Camper Name</div>
-                <div id="recent-points"> Points in the Past 30 Days</div>
-                <div id="total-points"> All Time Points</div>
+                <div id="recent-points"> <button onClick={this.props.displayRecent}> Last 30 Days Points </button></div>
+                <div id="total-points"> <button onClick={this.props.displayAllTime}> All Time Points </button> </div>
             </div>
         )
     }
 });
 
-
 var CamperData = React.createClass({
-    //make API call
     render: function(){
-        //all time data
-        axios.get('http://fcctop100.herokuapp.com/api/fccusers/top/alltime')
-            .then(function(response){
-                console.log("all time")
-                console.log(response)
-            })
-            .catch(function(error){
-                console.log(error)
-            });
-        //last 30 days data
-        axios.get('http://fcctop100.herokuapp.com/api/fccusers/top/recent')
-            .then(function(response){
-                console.log("recent")
-                console.log(response)
-            })
-            .catch(function(error){
-                console.log(error)
-            });
         return(
-        <div id="board"> Camper Data</div>
+            //loop over array of camper data rows - with array .map  from data array
+            // two arrays in this.state.recent, this.state.allTime
+            // this.state[this.state.active].map
+        <div id="board">
+            <div className="camper-row">{this.props.rank} {this.props.img} {this.props.username} {this.props.recent} {this.props.alltime}</div>
+        </div>
         )
     }
-    //loop over camper data and display- each camper as competent that displays?
 
-    //toggle between last 30 days and all time
+
+    //toggle between last 30 days and all time -on click using state, when clicking on 30days label or on Alltime label
 });
 
 
 //Main competent rendering all components
 var LeaderBoard = React.createClass({
+    getIntialState: function(){
+        return{
+            allTime: [],
+            recent: [],
+            active: 'allTime'
+        }
+    },
+    componentDidMount: function() {
+        let component = this;
+        axios.get('http://fcctop100.herokuapp.com/api/fccusers/top/alltime').then(function(allTime){
+            axios.get('http://fcctop100.herokuapp.com/api/fccusers/top/recent').then(function(recent){
+                component.setState({
+                    recent: recent.data,
+                    allTime: allTime.data
+                });
+            })
+        }).catch(function(err){console.log(err)});
+    },
+    displayAllTime: function(){
+        console.log("display all time points");
+       this.state.active = 'allTime'
+    },
+    displayRecent: function(){
+        console.log("display recent points");
+        this.state.active = 'recent'
+    },
     render: function(){
         return(
             <div>
                 <div id="board-title"> Camper Leaderboard </div>
-                <BoardLabels/>
+                <BoardLabels displayAllTime={this.displayAllTime} displayRecent={this.displayRecent} />
                 <CamperData/>
             </div>
         )
@@ -66,4 +80,4 @@ var LeaderBoard = React.createClass({
 
 
 //render components
-ReactDOM.render(<LeaderBoard/>, document.getElementById('board'));
+ReactDOM.render(<LeaderBoard/>, document.getElementById("app"));
